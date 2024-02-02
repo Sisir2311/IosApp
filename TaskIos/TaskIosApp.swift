fe78c664836260e01f35ac52b595a507354dc183
Kavya Goljana <113962209+KavyaGoljana@users.noreply.github.com>
2024-02-02 11:06:24 +0530

    Initial Commit

---
+++ TaskIos/TaskIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskIosApp.swift
+//  TaskIos
+//
+//  Created by Sisir Goljana on 02/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
